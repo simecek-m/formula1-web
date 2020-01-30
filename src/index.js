b150926b08808d80df01661df86f065f5f985261
Martin Šimeček <simecek.m@seznam.cz>
2020-01-30 10:47:14 +0100

    create ApolloClient and try test query

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient, { gql } from 'apollo-boost';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8080/graphql'
+});
+
+apolloClient
+  .query({
+    query: gql`
+    {
+      drivers {
+        firstname
+        lastname
+      }
+      teams {
+        name
+        principal
+      }
+    }`
+  })
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
