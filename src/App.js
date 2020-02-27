ff09dd09ab7ee3aa563cfb191175874524f27caa
Martin Šimeček <simecek.m@seznam.cz>
2020-02-27 17:13:27 +0100

    use Router and Navigation components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./App.css";
-import styled from "styled-components";
-
-const StyledLine = styled.div`
-  color: ${props => props.color || props.theme.fn};
-  background: ${props => props.background || props.theme.bg};
-  font-weight: ${props => (props.bold ? "bold" : "normal")};
-  padding: 15px;
-  text-align: center;
-`;
+import Navigation from "component/navigation/Navigation";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "component/page/About";
+import NotFound from "component/page/NotFound";
+import Home from "component/page/Home";
+import "animate.css";
 
 function App() {
-  return <StyledLine>Visual primitives for the component age.</StyledLine>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/404" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
