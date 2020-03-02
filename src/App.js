650b3d2aef7956a93a7dfa36308fdf4e5d3c69cd
Martin Šimeček <simecek.m@seznam.cz>
2020-03-02 14:00:42 +0100

    delete About page component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Navigation from "component/navigation/Navigation";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import About from "component/page/About";
 import NotFound from "component/page/NotFound";
 import Drivers from "component/page/Drivers";
 import "animate.css";
@@ -13,7 +12,6 @@ function App() {
       <Navigation />
       <Switch>
         <Route exact path="/" component={Drivers} />
-        <Route path="/about" component={About} />
         <Route path={["/404", "*"]} component={NotFound} />
       </Switch>
     </BrowserRouter>
--- src/component/page/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About() {
-  return <div className="animated fadeIn">ABOUT</div>;
-}
-
-export default About;
