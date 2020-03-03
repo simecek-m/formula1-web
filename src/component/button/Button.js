74e3aa499673873ea476a54a8fcaacb8d0c5583f
Martin Šimeček <simecek.m@seznam.cz>
2020-03-03 14:55:22 +0100

    add Button component

---
+++ src/component/button/Button.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "theme";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StyledButton = styled.button`
+  color: ${props => props.color};
+  font-size: ${props => props.size};
+  border: 4px solid ${props => props.color};
+  font-weight: bold;
+  margin: 10px auto;
+  padding: 15px 20px;
+  background: none;
+  cursor: pointer;
+  outline: none;
+  display: block;
+  transition: 0.5s ease all;
+  :hover {
+    color: ${props => (props.dark ? "white" : "black")};
+    background: ${props => props.color};
+  }
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  margin-right: 10px;
+`;
+
+function Button({
+  icon = null,
+  children,
+  color = theme.fg,
+  size = "22px",
+  dark = false,
+  onClick
+}) {
+  const buttonIcon = icon ? <StyledIcon icon={icon} /> : null;
+  return (
+    <StyledButton onClick={onClick} color={color} size={size} dark={dark}>
+      {buttonIcon}
+      {children.toUpperCase()}
+    </StyledButton>
+  );
+}
+
+export default Button;
