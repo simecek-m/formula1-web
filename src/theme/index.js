089f647b079dfd810e9508490f03da70349da7a8
Martin Šimeček <simecek.m@seznam.cz>
2020-03-03 14:56:17 +0100

    add status colors to theme constants

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,13 @@
 const theme = {
   fg: "#ededed",
   bg: "#242020",
-  contrast: "#383838"
+  contrast: "#383838",
+  status: {
+    info: "#33a4f5",
+    success: "#68F330",
+    warning: "#F9B026",
+    danger: "#FF3939"
+  }
 };
 
 export default theme;
