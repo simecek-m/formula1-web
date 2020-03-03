8a903c27d84c3b8111d731a8132854dd38a1bc2f
Martin Šimeček <simecek.m@seznam.cz>
2020-03-03 13:51:59 +0100

    add default application style

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NotFound from "component/page/NotFound";
 import Drivers from "component/page/Drivers";
 import "animate.css";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background-color: ${props => props.theme.contrast};
+  color: ${props => props.theme.fg};
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Switch>
-        <Route exact path="/" component={Drivers} />
-        <Route path={["/404", "*"]} component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <StyledApp>
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Drivers} />
+          <Route path={["/404", "*"]} component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </StyledApp>
   );
 }
 
