e8208f075eaa4094d3c8f5bd2d3820ea5e9852cf
Martin Šimeček <simecek.m@seznam.cz>
2020-03-06 14:58:00 +0100

    add Driver detail component

--- src/component/page/Driver.js
+++ src/component/page/Driver.js
@@ -2,27 +2,61 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import Loading from "component/page/Loading";
+import Error from "component/page/Error";
+import styled from "styled-components";
+import { SERVER_HOST, SERVER_PORT } from "constant";
 
 const DRIVER_QUERY = gql`
   query($id: String!) {
     driver(id: $id) {
       firstname
       lastname
+      birthday
+      height(unit: METER)
+      position
+      points
+      number
+      picture
+      team {
+        name
+      }
+      country {
+        name
+        flag
+      }
     }
   }
 `;
 
+const StyledDriverDetail = styled.div``;
+
+const Avatar = styled.img`
+  height: 500px;
+  width: 300px;
+  object-fit: cover;
+`;
+
+const Text = styled.div``;
+
 function Driver() {
   const { id } = useParams();
-  const { data } = useQuery(DRIVER_QUERY, {
-    variables: { id: "5e5d209be560b89d0e937690" }
+  const { loading, error, data } = useQuery(DRIVER_QUERY, {
+    variables: { id }
   });
-  console.log("data: ", data);
-  return (
-    <div>
-      <div>driver id: {id}</div>
-      <div>data: {JSON.stringify(data)}</div>
-    </div>
+  console.log(data);
+  const driver = data ? data.driver : null;
+  return loading ? (
+    <Loading />
+  ) : error ? (
+    <Error />
+  ) : (
+    <StyledDriverDetail>
+      <Avatar src={`${SERVER_HOST}:${SERVER_PORT}${driver.picture}`} />
+      <Text>{driver.firstname}</Text>
+      <Text>{driver.lastname}</Text>
+      <Text></Text>
+    </StyledDriverDetail>
   );
 }
 
