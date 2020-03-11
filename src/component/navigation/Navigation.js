2db035719598d6bb57856a1ea176524ff4ace287
Martin Šimeček <simecek.m@seznam.cz>
2020-03-11 09:36:28 +0100

    Navigation component simplification

--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import NavigationItem from "component/navigation/NavigationItem";
 import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 
 const StyledNavigation = styled.div`
   color: ${props => props.theme.fg};
@@ -12,6 +12,21 @@ const StyledNavigation = styled.div`
   position: fixed;
   width: 100%;
   box-shadow: 0 5px 10px 0 ${props => props.theme.bg};
+`;
+
+const NavigationItem = styled(NavLink)`
+  color: ${props => props.theme.fg};
+  margin: 10px;
+  opacity: 0.7;
+  font-size: 20px;
+  text-align: center;
+  cursor: pointer;
+  transition: 0.3s ease all;
+  text-decoration: none;
+  :hover,
+  &.selected {
+    opacity: 1;
+  }
 `;
 
 function Navigation() {
