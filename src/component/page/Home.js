d2e913ef573f0f6dfa17049ebe9a967adef5e994
Martin Šimeček <simecek.m@seznam.cz>
2020-12-07 11:14:15 +0100

    change logo

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -4,14 +4,10 @@ import styled from "styled-components";
 const StyledHomePage = styled.div`
   margin: 0 auto;
   text-align: center;
-  background-image: url("/img/background.png");
   height: 100vh;
 `;
 
 const StyledCarImage = styled.img`
-  height: 80vh;
-  margin-top: 10vh;
-
 `;
 
 function Home() {
