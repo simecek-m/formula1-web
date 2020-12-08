179625666240e466c8e0c51f66054a9d37070720
Martin Šimeček <simecek.m@seznam.cz>
2020-12-08 15:57:30 +0100

    use Blink component on Home page

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,19 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import Blink from "component/blink/Blink";
 
 const StyledHomePage = styled.div`
   margin: 0 auto;
   text-align: center;
-  height: 100vh;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-const StyledCarImage = styled.img`
-`;
+const BLINK_CONTENT = [
+  {
+    text: "Passion",
+    type: "text",
+  }, {
+    src: "/img/gallery/hamilton.jpg",
+    type: "image"
+  }, {
+    text: "Power",
+    type: "text"
+  }, {
+    src: "/img/gallery/fly.jpg",
+    type: "image",
+  }, {
+    text: "Formula 1",
+    color: "#ba0000",
+    fontSize: "64px"
+  }
+]
 
 function Home() {
   return (
     <StyledHomePage>
-      <StyledCarImage src="/img/car.png" alt="logo-car"/>
+      <Blink content={BLINK_CONTENT}/>
     </StyledHomePage>
   );
 }
